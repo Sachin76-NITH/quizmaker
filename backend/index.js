58e2ee5c4901d09558df477022f0329071db52f9
Sachin76-NITH <20bec076@nith.ac.in>
2024-07-06 11:38:52 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://quizmaker-sachin.vercel.app/', // replace with your frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials if needed
+  }));
 
 // Routes
 app.use('/api/auth', authRoutes);
